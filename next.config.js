ed5b39929db7359b1f209eeea9101d34523be6bf
Emily Zhao <emily.zhao6@gmail.com>
2022-04-01 13:23:57 -0400

    add filter box to website

--- next.config.js
+++ next.config.js
@@ -6,8 +6,16 @@ module.exports = {
     }),
     config.module.rules.push({
       test: /\.ipynb$/,
+      exclude: /node_modules/,
       use: 'ipynb?cellsOnly=true'
-    })
+    }),
+    config.module.rules.push({
+      test: /\.html$/i,
+      use: "html-loader",
+    })   
     return config
   },
 }
+
+
+// https://github.com/n6g7/ipynb-loader
